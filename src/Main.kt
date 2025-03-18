534de9206939bd8f0aaeab51b1e0b46bd9a3d770
waimea-arcollier <arcollier@waimea.school.nz>
2025-03-18 14:48:02 +1300

    Setup done

--- src/Main.kt
+++ src/Main.kt
@@ -2,9 +2,9 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   Connectris
+ * Project Author: Amber Collier
+ * GitHub Repo:    https://github.com/waimea-arcollier/level-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
