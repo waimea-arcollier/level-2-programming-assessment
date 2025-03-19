2a4186d1dc46820702c73b84c6a0278348ff3c72
waimea-arcollier <arcollier@waimea.school.nz>
2025-03-20 12:26:28 +1300

    Began coding game
    made welcome message, wrote comments, started grid

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+
 /**
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
@@ -14,9 +15,35 @@
  * =====================================================================
  */
 
-
+const val EMPTY = "□"
+const val GRID_SIZE = 7
+const val COUNTER = "●"
+val counterP1 = COUNTER.blue()
+val counterP2 = COUNTER.red()
 
 fun main() {
-    println("Hello World!")
+    //welcome message
+    val title = "Welcome to Connect 4!!"
+    print("$counterP1-$counterP2-".repeat(title.length /3))
+    println (counterP1)
+    println("$counterP2   ${title.blue().bold()}")
+    print("$counterP1-$counterP2-".repeat(title.length /3))
+    println (counterP1)
+
+    //rules
+    //setup grid
+    //player turns loop
+    //win message
 }
+/**
+ * Function to set up the grid (incomplete)
+ */
+fun setupGrid(): MutableList<String> {
+    val gridList = mutableListOf<String>()
+    for (i in 1..GRID_SIZE) cageList.add(EMPTY)
+    return gridList
+}
+/**
+ * Function to check whether there are four of a players counters in a line
+ */
 
